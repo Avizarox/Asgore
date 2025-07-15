f3a5614dad84d3f09594f0e93a8513b76544af8f
Avizarox <garmashov.matvey@yandex.ru>
2025-07-15 23:11:03 +0700

    basic movement and camera

---
+++ camera_3d.gd
@@ -0,0 +1,7 @@
+extends Camera3D
+
+@export var player:CharacterBody3D
+@export var offset:Vector3
+
+func _physics_process(delta):
+	position = lerp(position, player.position + offset, 0.1)
---
+++ movement.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+@export var speed:float = 2
+@export var fall_acceleration:float = 100
+
+var target_velocity:Vector3 = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction:Vector3 = Vector3.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_left"):
+		direction.x += 1
+	if Input.is_action_pressed("move_up"):
+		direction.z += 1
+	if Input.is_action_pressed("move_down"):
+		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized() * transform.basis
+		$MeshInstance3D.look_at(position + direction, Vector3.UP)
+	
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if !is_on_floor():
+		target_velocity.y -= fall_acceleration * delta
+	
+	velocity = target_velocity
+	move_and_slide()
+	
+	
